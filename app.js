1fde40b6b332150757bf6dfd1474501d25046d66
thomas miller <thomas.miller.forbusiness@gmail.com>
2019-10-20 12:33:01 +0330

    navbar html = done

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-$('.filter-basic').mdbFilter();
+window.onscroll = () => {
+    const nav = document.querySelector('#my-navbar');
+    if(this.scrollY <= 10) nav.className = 'navbar navbar-expand-lg navbar-light bg-light fixed-top'; else nav.className = 'navbar navbar-expand-lg navbar-dark bg-dark fixed-top';
+  };
