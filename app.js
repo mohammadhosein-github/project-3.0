f1537e3a0e229420b4327d381fab5522ee92172f
thomas miller <thomas.miller.forbusiness@gmail.com>
2019-10-21 16:27:13 +0330

    active class for filter button is done

--- app.js
+++ app.js
@@ -4,3 +4,33 @@ window.onscroll = () => {
   if(this.scrollY <= 10) nav.className = 'navbar navbar-expand-lg navbar-light bg-light fixed-top'; else nav.className = 'navbar navbar-expand-lg navbar-dark bg-dark fixed-top';
 };
 
+// (function(){
+//   'use strict';
+
+//   $('.filter-nav > button').on('click', function(e){
+//     e.preventDefault();
+//     $('.filter-nav > button').removeClass('active');
+//     $(this).addClass('active');
+//   })
+// })(jQuery);
+
+
+const filterBtns = document.querySelectorAll('.filter-nav > button');
+
+for(const cur of filterBtns){
+  cur.addEventListener('click', function(){
+    for(const elem of filterBtns){
+      elem.classList.remove('active');
+    }
+    cur.classList.add('active');
+  });
+}
+
+
+
+
+
+
+
+
+
