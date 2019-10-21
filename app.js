81c79a0a074e7ea5f8c104fbd65cce08a30aeaae
thomas miller <thomas.miller.forbusiness@gmail.com>
2019-10-21 08:58:55 +0330

    -

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+// ------ Navbar Background Chagne On Scroll ------
 window.onscroll = () => {
-    const nav = document.querySelector('#my-navbar');
-    if(this.scrollY <= 10) nav.className = 'navbar navbar-expand-lg navbar-light bg-light fixed-top'; else nav.className = 'navbar navbar-expand-lg navbar-dark bg-dark fixed-top';
-  };
+  const nav = document.querySelector('#my-navbar');
+  if(this.scrollY <= 10) nav.className = 'navbar navbar-expand-lg navbar-light bg-light fixed-top'; else nav.className = 'navbar navbar-expand-lg navbar-dark bg-dark fixed-top';
+};
+
