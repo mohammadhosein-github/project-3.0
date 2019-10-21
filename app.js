c7e8425492005ad3606474ffa0e9d84706144bf0
thomas miller <thomas.miller.forbusiness@gmail.com>
2019-10-21 16:28:56 +0330

    app.js save

--- app.js
+++ app.js
@@ -4,15 +4,7 @@ window.onscroll = () => {
   if(this.scrollY <= 10) nav.className = 'navbar navbar-expand-lg navbar-light bg-light fixed-top'; else nav.className = 'navbar navbar-expand-lg navbar-dark bg-dark fixed-top';
 };
 
-// (function(){
-//   'use strict';
 
-//   $('.filter-nav > button').on('click', function(e){
-//     e.preventDefault();
-//     $('.filter-nav > button').removeClass('active');
-//     $(this).addClass('active');
-//   })
-// })(jQuery);
 
 
 const filterBtns = document.querySelectorAll('.filter-nav > button');
